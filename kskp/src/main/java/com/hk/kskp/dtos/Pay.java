9490ac10d2a95b94f47093fba9afbb578d10fd46
hkedu <hkedu@192.168.2.106>
2019-10-22 10:27:18 +0900

    commit

---
+++ kskp/src/main/java/com/hk/kskp/dtos/Pay.java
@@ -0,0 +1,133 @@
+package com.hk.kskp.dtos;
+
+public class Pay {
+	
+	private int p_seq;
+	private int g_seq;
+	private int m_seq;
+	private int p_cost;
+	private int p_num;
+	private String p_date;
+	private String p_name;
+	private String p_conts;
+	private String p_img;
+	private String p_meet;
+	private String p_flag;
+	
+	public Pay() {
+		super();
+	}
+
+	public Pay(int p_seq, int g_seq, int m_seq, int p_cost, int p_num, String p_date, String p_name, String p_conts,
+			String p_img, String p_meet, String p_flag) {
+		super();
+		this.p_seq = p_seq;
+		this.g_seq = g_seq;
+		this.m_seq = m_seq;
+		this.p_cost = p_cost;
+		this.p_num = p_num;
+		this.p_date = p_date;
+		this.p_name = p_name;
+		this.p_conts = p_conts;
+		this.p_img = p_img;
+		this.p_meet = p_meet;
+		this.p_flag = p_flag;
+	}
+
+	public int getP_seq() {
+		return p_seq;
+	}
+
+	public void setP_seq(int p_seq) {
+		this.p_seq = p_seq;
+	}
+
+	public int getG_seq() {
+		return g_seq;
+	}
+
+	public void setG_seq(int g_seq) {
+		this.g_seq = g_seq;
+	}
+
+	public int getM_seq() {
+		return m_seq;
+	}
+
+	public void setM_seq(int m_seq) {
+		this.m_seq = m_seq;
+	}
+
+	public int getP_cost() {
+		return p_cost;
+	}
+
+	public void setP_cost(int p_cost) {
+		this.p_cost = p_cost;
+	}
+
+	public int getP_num() {
+		return p_num;
+	}
+
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
+	}
+
+	public String getP_date() {
+		return p_date;
+	}
+
+	public void setP_date(String p_date) {
+		this.p_date = p_date;
+	}
+
+	public String getP_name() {
+		return p_name;
+	}
+
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+
+	public String getP_conts() {
+		return p_conts;
+	}
+
+	public void setP_conts(String p_conts) {
+		this.p_conts = p_conts;
+	}
+
+	public String getP_img() {
+		return p_img;
+	}
+
+	public void setP_img(String p_img) {
+		this.p_img = p_img;
+	}
+
+	public String getP_meet() {
+		return p_meet;
+	}
+
+	public void setP_meet(String p_meet) {
+		this.p_meet = p_meet;
+	}
+
+	public String getP_flag() {
+		return p_flag;
+	}
+
+	public void setP_flag(String p_flag) {
+		this.p_flag = p_flag;
+	}
+
+	@Override
+	public String toString() {
+		return "Pay [p_seq=" + p_seq + ", g_seq=" + g_seq + ", m_seq=" + m_seq + ", p_cost=" + p_cost + ", p_num="
+				+ p_num + ", p_date=" + p_date + ", p_name=" + p_name + ", p_conts=" + p_conts + ", p_img=" + p_img
+				+ ", p_meet=" + p_meet + ", p_flag=" + p_flag + "]";
+	}
+	
+
+}
---
+++ kskp/src/main/java/com/hk/kskp/dtos/Review.java
@@ -0,0 +1,127 @@
+package com.hk.kskp.dtos;
+
+import java.sql.Date;
+
+public class Review {
+
+	
+	private int r_seq;
+	private int m_seq;
+	private int g_seq;
+	private String r_conts;
+	private Date r_regdate;
+	private String r_img;
+	private int r_star;
+	private String r_ans;
+	private int r_guflag;
+	private int r_falg;
+	
+	public Review() {
+		super();
+	}
+
+	public Review(int r_seq, int m_seq, int g_seq, String r_conts, Date r_regdate, String r_img, int r_star,
+			String r_ans, int r_guflag, int r_falg) {
+		super();
+		this.r_seq = r_seq;
+		this.m_seq = m_seq;
+		this.g_seq = g_seq;
+		this.r_conts = r_conts;
+		this.r_regdate = r_regdate;
+		this.r_img = r_img;
+		this.r_star = r_star;
+		this.r_ans = r_ans;
+		this.r_guflag = r_guflag;
+		this.r_falg = r_falg;
+	}
+
+	public int getR_seq() {
+		return r_seq;
+	}
+
+	public void setR_seq(int r_seq) {
+		this.r_seq = r_seq;
+	}
+
+	public int getM_seq() {
+		return m_seq;
+	}
+
+	public void setM_seq(int m_seq) {
+		this.m_seq = m_seq;
+	}
+
+	public int getG_seq() {
+		return g_seq;
+	}
+
+	public void setG_seq(int g_seq) {
+		this.g_seq = g_seq;
+	}
+
+	public String getR_conts() {
+		return r_conts;
+	}
+
+	public void setR_conts(String r_conts) {
+		this.r_conts = r_conts;
+	}
+
+	public Date getR_regdate() {
+		return r_regdate;
+	}
+
+	public void setR_regdate(Date r_regdate) {
+		this.r_regdate = r_regdate;
+	}
+
+	public String getR_img() {
+		return r_img;
+	}
+
+	public void setR_img(String r_img) {
+		this.r_img = r_img;
+	}
+
+	public int getR_star() {
+		return r_star;
+	}
+
+	public void setR_star(int r_star) {
+		this.r_star = r_star;
+	}
+
+	public String getR_ans() {
+		return r_ans;
+	}
+
+	public void setR_ans(String r_ans) {
+		this.r_ans = r_ans;
+	}
+
+	public int getR_guflag() {
+		return r_guflag;
+	}
+
+	public void setR_guflag(int r_guflag) {
+		this.r_guflag = r_guflag;
+	}
+
+	public int getR_falg() {
+		return r_falg;
+	}
+
+	public void setR_falg(int r_falg) {
+		this.r_falg = r_falg;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [r_seq=" + r_seq + ", m_seq=" + m_seq + ", g_seq=" + g_seq + ", r_conts=" + r_conts
+				+ ", r_regdate=" + r_regdate + ", r_img=" + r_img + ", r_star=" + r_star + ", r_ans=" + r_ans
+				+ ", r_guflag=" + r_guflag + ", r_falg=" + r_falg + "]";
+	}
+	
+	
+	
+}
