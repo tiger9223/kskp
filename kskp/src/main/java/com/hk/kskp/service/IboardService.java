c1093ae11560fb091fdc8455c22b8c6f2f892d34
jeh <mfmania2@naver.com>
2019-10-23 19:27:06 +0900

    commit

---
+++ kskp/src/main/java/com/hk/kskp/service/IboardService.java
@@ -0,0 +1,42 @@
+package com.hk.kskp.service;
+
+import java.util.List;
+
+import com.hk.kskp.dtos.NoticeDto;
+import com.hk.kskp.dtos.QaDto;
+
+public interface IboardService {
+	
+	//공지게시판글 전체보기
+	public List<NoticeDto> NgetAllList();
+	
+	//공지게시판 글 추가하기
+	public boolean NinsertBoard(NoticeDto dto);
+	
+	//공지게시판 글 상세보기
+	public NoticeDto NgetBoard(int seq);
+	
+	//공지게시판 글 수정하기
+	public boolean NupdateBoard(NoticeDto dto);
+	
+	//공지게시판 글 지우기
+	public boolean NdelBoard(int seq);
+	
+	//Q&A 글 전체보기
+	public List<QaDto> QgetAllList();
+	
+	//Q&A 글 추가하기
+	public boolean QinsertBoard(QaDto dto);
+	
+	//Q&A 글 상세보기
+	public QaDto QgetBoard(int seq);
+	
+	//Q&A 글 수정하기
+	public boolean QupdateBoard(QaDto dto);
+	
+	//Q&A 글 삭제하기
+	public boolean QdelBoard(int seq);
+	
+	
+
+}
---
+++ kskp/src/main/java/com/hk/kskp/service/boardService.java
@@ -0,0 +1,70 @@
+package com.hk.kskp.service;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hk.kskp.daos.IboardDao;
+import com.hk.kskp.dtos.NoticeDto;
+import com.hk.kskp.dtos.QaDto;
+
+@Service
+public class boardService implements IboardService{
+	//service객체에서는 Dao객체를 가져다 쓴다.
+	
+	@Autowired
+	private IboardDao boardDao;
+
+	@Override
+	public List<NoticeDto> NgetAllList() {
+		return boardDao.NgetAllList();
+	}
+
+	@Override
+	public boolean NinsertBoard(NoticeDto dto) {
+		return boardDao.NinsertBoard(dto);
+	}
+
+	@Override
+	public NoticeDto NgetBoard(int seq) {
+		return boardDao.NgetBoard(seq);
+	}
+
+	@Override
+	public boolean NupdateBoard(NoticeDto dto) {
+		return boardDao.NupdateBoard(dto);
+	}
+
+	@Override
+	public boolean NdelBoard(int seq) {
+		return boardDao.NdelBoard(seq);
+	}
+
+	@Override
+	public List<QaDto> QgetAllList() {
+		return boardDao.QgetAllList();
+	}
+
+	@Override
+	public boolean QinsertBoard(QaDto dto) {
+		return boardDao.QinsertBoard(dto);
+	}
+
+	@Override
+	public QaDto QgetBoard(int seq) {
+		return boardDao.QgetBoard(seq);
+	}
+
+	@Override
+	public boolean QupdateBoard(QaDto dto) {
+		return boardDao.QupdateBoard(dto);
+	}
+
+	@Override
+	public boolean QdelBoard(int seq) {
+		return boardDao.QdelBoard(seq);
+	}
+
+}
