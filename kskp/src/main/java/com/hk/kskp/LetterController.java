6e1aae4abc02ebe8533b2fe59973eff89a52b2af
hk_edu <hk_edu@192.168.2.103>
2019-10-25 15:55:07 +0900

    dd

--- kskp/src/main/java/com/hk/kskp/LetterController.java
+++ kskp/src/main/java/com/hk/kskp/LetterController.java
@@ -6,10 +6,16 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hk.kskp.dtos.LetterDto;
+import com.hk.kskp.dtos.NoticeDto;
+import com.hk.kskp.service.ILetterService;
+import com.hk.kskp.service.LetterService;
 
 /**
  * Handles requests for the application home page.
@@ -19,27 +25,29 @@ public class LetterController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LetterController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	@Autowired
+	private ILetterService LetterService;
 	
 	@RequestMapping(value="/sendLetterform.do",method = RequestMethod.GET)
-	public String insertForm() {
+	public String sendLetterform() {
 		logger.info("쪽지쓰기폼으로 이동");
 		return "sendLetter";
 	}
+	
+	@RequestMapping(value="sendletter.do",method = {RequestMethod.POST,RequestMethod.GET})
+	 public String sendLetter(Model model,LetterDto dto) {
+		
+		logger.info("공지게시판 글 추가하기");
+		boolean isS = LetterService.sendLetter(dto);
+		if(isS) {
+			return "redirect:letterlist.do";
+		}
+			return "letterlist";
+		}
+	
+	
+	
+	
+	
 	
 }
